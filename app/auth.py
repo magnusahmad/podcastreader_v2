8d5e2d13ecc512c1e9c23393359dd87867285fc0
magnusahmad <magnus.ahmad@gmail.com>
2024-02-07 19:20:26 +0100

    auth

--- app/auth.py
+++ app/auth.py
@@ -1,16 +1,21 @@
 from flask import session, request, redirect
 import pyrebase 
 import app
+import firebase_admin
+from firebase_admin import credentials, auth
 
-config = {   'apiKey': "AIzaSyA5tPUV7Xfg3KdQDjTy9hsVhBLjZWgEiaM",
-    'authDomain': "podcastreader-7e348.firebaseapp.com",
-    'projectId': "podcastreader-7e348",
-    'storageBucket': "podcastreader-7e348.appspot.com",
-    'messagingSenderId': "482563787442",
-    'appId': "1:482563787442:web:682fb4c900daa880bf97ab",
-    'databaseURL': ""
-}
+# config = {   'apiKey': "AIzaSyA5tPUV7Xfg3KdQDjTy9hsVhBLjZWgEiaM",
+#     'authDomain': "podcastreader-7e348.firebaseapp.com",
+#     'projectId': "podcastreader-7e348",
+#     'storageBucket': "podcastreader-7e348.appspot.com",
+#     'messagingSenderId': "482563787442",
+#     'appId': "1:482563787442:web:682fb4c900daa880bf97ab",
+#     'databaseURL': ""
+# }
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-app.secret_key = 'secret'
+# firebase = pyrebase.initialize_app(config)
+# auth = firebase.auth()
+# app.secret_key = 'secret'
+
+cred = credentials.Certificate('podcastreader-firebase-key.json')
+firebase_admin.initialize_app(cred)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,25 @@
 from flask import render_template, request, flash, get_flashed_messages, session, redirect, url_for
+import firebase_admin
+from firebase_admin import credentials, auth
 from app import app, db
 from download_youtube import * 
 from models import Shop_Button
 from datetime import datetime
 import auth
+import jsonify
+
+class convert_to_dot_notation(dict):
+    """
+    Access dictionary attributes via dot notation
+    """
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 @app.route("/")
 def homepage():
-    if session['user']:
+    if 'user' in session.keys():
         return render_template("index.html", user=session['user'])
     else:
         return render_template("index.html")
@@ -61,3 +73,43 @@ def login():
                 else:
                     return f'Login failed due to unknown reason'
     return render_template('login.html')
+
+cred = credentials.Certificate('podcastreader-firebase-key.json')
+# firebase_admin.initialize_app(cred)
+
+@app.route("/magic_link_login", methods=["POST"])
+def magic_link_login():
+    if ('user' in session):
+        return f'<p>Hi, {session["user"]}</p>'
+    if request.method == "POST":
+        email = request.form.get('email')
+        print(f'email: {email}, file=sys.stderr)')
+        action_code_settings = {
+            'url': 'http://localhost:8080/complete_sign_in'
+            }
+        action_code_settings = convert_to_dot_notation(action_code_settings)
+        try:
+            link = auth.auth.generate_sign_in_with_email_link(email, action_code_settings)
+            print(f'email: {email}')
+            # session['user'] = email
+            response = f"""
+            <p class="fade-in-text">Check your email for a sign in link! \n {link}</p>
+            """
+            return response
+        except Exception as e:
+            return f'Login failed: {e}'
+
+@app.route('/complete_sign_in', methods=['GET'])
+def complete_sign_in():
+    email = request.args.get('email')
+    link = request.args.get('link')
+                            
+    try:
+        # Verify the link and sign in the user
+        # This would typically be part of your client-side logic
+        # Here, for simplicity, we're just verifying the link
+        #result = auth.auth.sign_in_with_email_link(email, link)
+        return f'Email: {email} \n Link: {link}'
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
